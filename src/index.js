cc5afb48f7a7ecb87f4ab157ccd9f07a1ea694a4
PaulaQR <mryquin@gmail.com>
2022-01-26 11:47:02 -0500

    agregamos value de tarjeta en el boton

--- src/index.js
+++ src/index.js
@@ -4,21 +4,20 @@ console.log(validator);
 //Selectores
 
 const nombreDeUsuario = document.getElementById("nombreDeUsuario").value;
-const numeroDeTarjeta = document.getElementById("numeroDeTarjeta").value;
+const numeroDeTarjeta = document.getElementById("numeroDeTarjeta");
 
 //Eventos
 
 //Boton que  genera una alerta depende el dato que se ingrese, si es valido o invalido
-
-
 
 (() => {
   let botonValidar = document.getElementById("botonValidar");
 
   botonValidar.addEventListener("click", () => {
 
-    document.getElementById("contenedorTarjeta").style.display = "none";
+    validator.isValid(numeroDeTarjeta.value)
 
+    document.getElementById("contenedorTarjeta").style.display = "none";
     document.getElementById("segundaPantalla").style.display = "block";
   });
   
--- src/validator.js
+++ src/validator.js
@@ -5,29 +5,28 @@ y una propiedad es una asociación entre un nombre (o clave) y un valor.
 El valor de una propiedad puede ser una función, en cuyo caso la propiedad es conocida como un método.
 Además de los objetos que están predefinidos en el navegador, puedes definir tus propios objetos.
 */
-function validator(numero_tarjeta){
-  let cadena = this.numero_tarjeta;
-  this.longitud = cadena.length; 
-  this.cifra = null;
-  this.cifra_cad= null;
-  this.suma = 0;
+function isValid(numero_tarjeta){
+  let  longitud = numero_tarjeta.length; 
+  let cifra = null;
+  let cifra_cad= null;
+  let suma = 0;
   //Aqui aprendimos que son las expresiones regulares e HTML 
   // El método test() ejecuta la búsqueda de una ocurrencia entre una expresión regular y una cadena especificada. Devuelve true o false.
-  this.valid = false;
-  this.numValid = /[^0-9-\s]+/.test(numero_tarjeta); 
+  let valid = false;
+  let numValid = /[^0-9-\s]+/.test(numero_tarjeta); 
 
-  this.isvalid = () => {
-    if (this.numValid === true) {
+  let isvalid = () => {
+    if (numValid === true) {
       return alert("Ingrese solo caracteres de tipo numerico en este campo");
     }
 } 
 
 // Busca de manera general del 0 al 9 y si no esta lo elimina 
-cadena = cadena.replace(/\D/g, "");
+numero_tarjeta = numero_tarjeta.replace(/\D/g, "");
 
       //Para invertir numeros vamos a utlizar un for
-      for (let i = cadena.length - 1; i >= 0; i--){
-        suma +=  cadena.charAt(i);
+      for (let i = numero_tarjeta.length - 1; i >= 0; i--){
+        suma +=  numero_tarjeta.charAt(i);
       }
       /*
         charAt()
@@ -39,7 +38,7 @@ cadena = cadena.replace(/\D/g, "");
 
       //Recorremos el elemento numero_Tarjeta 
       for (let i=0; i < longitud; i+=2){
-        cifra = parseInt(cadena.charAt(i))*2;
+        cifra = parseInt(numero_tarjeta.charAt(i))*2;
         //parseInt Convierte (parsea) un argumento de tipo cadena y devuelve un entero de la base especificada.
         //escoge numeros de posicion impar 
 
@@ -55,7 +54,7 @@ cadena = cadena.replace(/\D/g, "");
     //sumar digitos y nuevos digitos 
 
     for (let  i=1; i < longitud; i+=2){
-      suma += parseInt(cadena.charAt(i));
+      suma += parseInt(numero_tarjeta.charAt(i));
     }
   
     //validacion 
@@ -64,6 +63,26 @@ cadena = cadena.replace(/\D/g, "");
     } else {
      alert("El número de tarjeta no es válido");
     }
-   }
+   
+
+   
+  }
+  
+  const validator = {
+    isValid,maskify
+  }
+
+  function maskify(numero_tarjeta){
+    
+
+    //Extraen los ultimos tres numeros 
+    let ultimosNumeros = numero_tarjeta.substring(numero_tarjeta.length-4);
+    //Se estraen todos menos los 3 ultimos 
+    let inicioNumeros = numero_tarjeta.substring(0,numero_tarjeta.length-4);
+    //Se enmascara con #  cualquier caracter 
+    let numerosSegundaPantalla = inicioNumeros.replace(/./g, '#') +  " " +  ultimosNumeros;
+    //retorna un objeto
+    return {ultimosNumeros, inicioNumeros, numerosSegundaPantalla};
+    }
 
 export default validator;
