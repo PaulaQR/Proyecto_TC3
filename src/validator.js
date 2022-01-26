cdbcca65a3eb35d0b10923b88fb3f6433d44e1da
PaulaQR <mryquin@gmail.com>
2022-01-26 08:55:13 -0500

    Input type number agregado

--- src/validator.js
+++ src/validator.js
@@ -5,9 +5,7 @@ y una propiedad es una asociación entre un nombre (o clave) y un valor.
 El valor de una propiedad puede ser una función, en cuyo caso la propiedad es conocida como un método.
 Además de los objetos que están predefinidos en el navegador, puedes definir tus propios objetos.
 */
-
 function validator(numero_tarjeta){
-
   let cadena = this.numero_tarjeta;
   this.longitud = cadena.length; 
   this.cifra = null;
@@ -17,6 +15,7 @@ function validator(numero_tarjeta){
   // El método test() ejecuta la búsqueda de una ocurrencia entre una expresión regular y una cadena especificada. Devuelve true o false.
   this.valid = false;
   this.numValid = /[^0-9-\s]+/.test(numero_tarjeta); 
+
   this.isvalid = () => {
     if (this.numValid === true) {
       return alert("Ingrese solo caracteres de tipo numerico en este campo");
@@ -24,29 +23,25 @@ function validator(numero_tarjeta){
 } 
 
 // Busca de manera general del 0 al 9 y si no esta lo elimina 
-
- cadena = cadena.replace(/\D/g, "");
-
+cadena = cadena.replace(/\D/g, "");
 
       //Para invertir numeros vamos a utlizar un for
       for (let i = cadena.length - 1; i >= 0; i--){
-        /*
+        suma +=  cadena.charAt(i);
+      }
+      /*
         charAt()
         Los caracteres de una cadena se indexan de izquierda a derecha.
         El índice del primer caracter es 0,
         y el índice del último caracter en una cadena llamada nombreCadena es nombreCadena.length - 1.
         Si el indice que usted proporciona está fuera del rango, JavaScript devuelve una cadena vacía.
         */
-        suma +=  cadena.charAt(i);
-      }
 
       //Recorremos el elemento numero_Tarjeta 
       for (let i=0; i < longitud; i+=2){
-
+        cifra = parseInt(cadena.charAt(i))*2;
         //parseInt Convierte (parsea) un argumento de tipo cadena y devuelve un entero de la base especificada.
         //escoge numeros de posicion impar 
-
-      cifra = parseInt(cadena.charAt(i))*2;
 
       //De la operacion anterior escoge los que sean mayores a 9  y los suma 
       if (cifra > 9 ){
@@ -70,9 +65,5 @@ function validator(numero_tarjeta){
      alert("El número de tarjeta no es válido");
     }
    }
-
-
-  
-
 
 export default validator;
