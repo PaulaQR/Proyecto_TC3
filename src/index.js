0863f837daff05591e1c0eafc7cd2f2695f766ff
PaulaQR <mryquin@gmail.com>
2022-01-31 15:33:21 -0500

    colocando condicion para que me permita colocar 14,15 y 16 caracteres

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ let botonValidar = document.getElementById("botonValidar");
     if(numeroFinal <= 16){
       return alert("La tarjeta tiene que tener minimo 14 numeros");
     }
-    //Nos  muestra los ultimos cuatro numeros luego de validar 
+    //Nos  muestra los ultimos cuatro numero 
   document.getElementById("ultimos-cuatro").innerHTML = validator.maskify(numeroFinal);
    let isValid = validator.isValid(numeroFinal)
 
